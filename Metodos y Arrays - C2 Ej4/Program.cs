cdd1f99fafa7d852288a0ad3740a0499148f5a83
Agneta <agnetagsu@yahoo.es>
2021-05-23 01:43:52 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej4
+{
+    class Factorial
+    {
+        public double calculafactorial(int numero)
+        {
+            double factorial = numero;
+            int i = numero;
+
+            do
+            {
+                i = i - 1;
+                factorial = factorial * i;
+            } while (i > 1);
+            return factorial;
+        }
+        static void Main(string[] args)
+        {
+            int numero;
+            Factorial elfactorial = new Factorial();
+            Console.WriteLine("Introduce un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(elfactorial.calculafactorial(numero));
+
+        }
+    }
+}
